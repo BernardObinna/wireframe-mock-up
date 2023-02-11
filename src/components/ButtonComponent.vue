b0cafa5c70ffafd705575291f60d184c6817737c
Vayne <bernardobinna2@gmail.com>
2023-02-11 15:37:56 +0100

    done with header

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="py-2 px-4" :class="classes">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+  props: {
+    text: {
+      type: String,
+    },
+    classes: {
+      type: String,
+    },
+  },
+};
+</script>
